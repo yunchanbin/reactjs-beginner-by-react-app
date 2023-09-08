ba4b2d138dab94b82a387383297e16c4a76b48b9
yunchanbin <21611273@yu.ac.kr>
2023-09-09 03:29:23 +0900

    7.0

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
-function App() {}
+import { useState } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState();
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Whrite your to do ..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
 
 export default App;
