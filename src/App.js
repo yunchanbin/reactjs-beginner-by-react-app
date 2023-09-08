d6e6f2e0740b6f45e536cd98bd1ad9bbc8e26694
yunchanbin <21611273@yu.ac.kr>
2023-09-09 01:49:24 +0900

    movie app start

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  function byFn() {
-    console.log("Bye :(");
-  }
-  function hiFn() {
-    console.log("created :)");
-  }
-
-  useEffect(() => {
-    hiFn();
-    return byFn();
-  }, []);
-  return <h1>Hello</h1>;
-}
-
-function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-    <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello></Hello> : null}
-    </div>
-  );
-}
+function App() {}
 
 export default App;
